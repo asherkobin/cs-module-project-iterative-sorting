a62af474eda3558138ac7172d54827ff8bfa8d77
Asher Kobin <Asher@Ashers-MBP.fios-router.home>
2020-05-19 16:20:01 -0400

    MVP + Stretch

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,24 @@
 def linear_search(arr, target):
-    # Your code here
-
-
-    return -1   # not found
-
+    for idx in range(len(arr)):
+      if arr[idx] == target:
+        return idx
+    
+    return -1 # not found
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+  first = 0
+  last = len(arr) - 1
+  middle = (first + last) // 2
 
-    # Your code here
+  while first <= last:
+    if arr[middle] < target:
+      first = middle + 1
+    elif arr[middle] == target:
+      return middle
+    else:
+      last = middle - 1
 
+    middle = (first + last) // 2
 
-    return -1  # not found
+  return -1 # not found
